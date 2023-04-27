83abbe19e7e72bb172243932255bba61589e164c
moath2elmarmori <moath222elmarmori@gmail.com>
2023-04-27 10:17:08 +0200

    sorting the projects

--- utils/projectsInfo.js
+++ utils/projectsInfo.js
@@ -1,22 +1,6 @@
 const projectsInfoArr = [
   {
     imgSrc:
-      "https://res.cloudinary.com/dk5awi1mn/image/upload/v1670593154/jbrish/screencapture-calculator-app-moath-netlify-app-2022-12-09-15_12_27_1_jupb3g.png",
-    imageAlt: "Calculator App",
-    projectTitle: "Calculator App",
-    projectLiveSite: "https://calculator-app-moath.netlify.app/",
-    projectGithubRepo: "https://github.com/moath2elmarmori/Calculator-App",
-  },
-  {
-    imgSrc:
-      "https://res.cloudinary.com/dk5awi1mn/image/upload/v1670597595/jbrish/screencapture-moath-yelpcamp-onrender-campgrounds-63871d4d1ca266fde58f3ad8-2022-12-09-16_51_38_gtheba.png",
-    imageAlt: "YelpCamp",
-    projectTitle: "YelpCamp",
-    projectLiveSite: "https://moath-yelpcamp.onrender.com/",
-    projectGithubRepo: "https://github.com/moath2elmarmori/yelpCamp",
-  },
-  {
-    imgSrc:
       "https://res.cloudinary.com/dk5awi1mn/image/upload/v1675934459/jbrish/Screenshot_1_vmveuo.png",
     imageAlt: "Entertainment Web App",
     projectTitle: "Entertainment Web App",
@@ -24,21 +8,22 @@ const projectsInfoArr = [
     projectGithubRepo:
       "https://github.com/moath2elmarmori/Entertainment-web-app",
   },
+
   {
     imgSrc:
-      "https://res.cloudinary.com/dk5awi1mn/image/upload/v1670592641/jbrish/screencapture-ip-address-tracker-moath-netlify-app-2022-12-09-15_29_57_uustbg.png",
-    imageAlt: "IP Address Tracker",
-    projectTitle: "IP Address Tracker",
-    projectLiveSite: "https://ip-address-tracker-moath.netlify.app/",
-    projectGithubRepo: "https://github.com/moath2elmarmori/IP-Address-Tracker",
+      "https://res.cloudinary.com/dk5awi1mn/image/upload/v1682579799/jbrish/Screenshot_2_tzyk19.png",
+    imageAlt: "Connect Four Game Screen Shot",
+    projectTitle: "Connect Four Game",
+    projectLiveSite: "https://connect-four-game-moath.vercel.app/",
+    projectGithubRepo: "https://github.com/moath2elmarmori/Connect-Four-Game",
   },
   {
     imgSrc:
-      "https://res.cloudinary.com/dk5awi1mn/image/upload/v1670594939/jbrish/screencapture-galleria-show-vercel-app-2022-12-09-16_07_35_mtk9m5.png",
-    imageAlt: "Galleria Show Site",
-    projectTitle: "Galleria Show Site",
-    projectLiveSite: "https://galleria-show.vercel.app/",
-    projectGithubRepo: "https://github.com/moath2elmarmori/galleria-show-site",
+      "https://res.cloudinary.com/dk5awi1mn/image/upload/v1670593154/jbrish/screencapture-calculator-app-moath-netlify-app-2022-12-09-15_12_27_1_jupb3g.png",
+    imageAlt: "Calculator App",
+    projectTitle: "Calculator App",
+    projectLiveSite: "https://calculator-app-moath.netlify.app/",
+    projectGithubRepo: "https://github.com/moath2elmarmori/Calculator-App",
   },
   {
     imgSrc:
@@ -50,20 +35,38 @@ const projectsInfoArr = [
   },
   {
     imgSrc:
+      "https://res.cloudinary.com/dk5awi1mn/image/upload/v1670594939/jbrish/screencapture-galleria-show-vercel-app-2022-12-09-16_07_35_mtk9m5.png",
+    imageAlt: "Galleria Show Site",
+    projectTitle: "Galleria Show Site",
+    projectLiveSite: "https://galleria-show.vercel.app/",
+    projectGithubRepo: "https://github.com/moath2elmarmori/galleria-show-site",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/dk5awi1mn/image/upload/v1670597595/jbrish/screencapture-moath-yelpcamp-onrender-campgrounds-63871d4d1ca266fde58f3ad8-2022-12-09-16_51_38_gtheba.png",
+    imageAlt: "YelpCamp",
+    projectTitle: "YelpCamp",
+    projectLiveSite: "https://moath-yelpcamp.onrender.com/",
+    projectGithubRepo: "https://github.com/moath2elmarmori/yelpCamp",
+  },
+
+  {
+    imgSrc:
+      "https://res.cloudinary.com/dk5awi1mn/image/upload/v1670592641/jbrish/screencapture-ip-address-tracker-moath-netlify-app-2022-12-09-15_29_57_uustbg.png",
+    imageAlt: "IP Address Tracker",
+    projectTitle: "IP Address Tracker",
+    projectLiveSite: "https://ip-address-tracker-moath.netlify.app/",
+    projectGithubRepo: "https://github.com/moath2elmarmori/IP-Address-Tracker",
+  },
+
+  {
+    imgSrc:
       "https://res.cloudinary.com/dk5awi1mn/image/upload/v1670596469/jbrish/screencapture-moath-ticketing-system-onrender-products-637dd21d71d505ae57d14a05-tickets-638b247ce9cb7936e744e2c4-2022-12-09-16_30_01_e3ihhk.png",
     imageAlt: "Ticketing System",
     projectTitle: "Ticketing System",
     projectLiveSite: "https://moath-ticketing-system.onrender.com/",
     projectGithubRepo:
       "https://github.com/moath2elmarmori/Ticketing-System-Project",
-  },
-  {
-    imgSrc:
-      "https://res.cloudinary.com/dk5awi1mn/image/upload/v1682579799/jbrish/Screenshot_2_tzyk19.png",
-    imageAlt: "Connect Four Game Screen Shot",
-    projectTitle: "Connect Four Game",
-    projectLiveSite: "https://connect-four-game-moath.vercel.app/",
-    projectGithubRepo: "https://github.com/moath2elmarmori/Connect-Four-Game",
   },
   // {
   //   imgSrc:
